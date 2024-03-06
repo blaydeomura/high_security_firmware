a4dd1cfa3464522c54d2c1ed9ba3ae193e33256b
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-03-06 13:57:09 -0800

    created error module

---
+++ src/error.rs
@@ -0,0 +1,34 @@
+use std::{error::Error, fmt};
+use ring::error::KeyRejected; 
+
+//Error handling
+#[derive(Debug)]
+pub enum MyError {
+    KeyRejected(KeyRejected),
+    Unspecified(ring::error::Unspecified), // Add this line
+    // expand on cases later
+}
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            MyError::KeyRejected(e) => write!(f, "Key rejected: {:?}", e),
+            MyError::Unspecified(_) => write!(f, "An unspecified error occurred"),
+            // Input other cases here
+        }
+    }
+}
+
+impl Error for MyError {}
+
+impl From<KeyRejected> for MyError {
+    fn from(err: KeyRejected) -> MyError {
+        MyError::KeyRejected(err)
+    }
+}
+
+impl From<ring::error::Unspecified> for MyError {
+    fn from(err: ring::error::Unspecified) -> MyError {
+        MyError::Unspecified(err)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod wallet;
 pub mod commands;
+pub mod error;
