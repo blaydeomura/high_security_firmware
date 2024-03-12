c69d63375bf0080578160167ab5bf6266e74beed
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-03-12 00:26:01 -0700

    created persona module and removed ciphersuite module

--- src/persona.rs
+++ src/persona.rs
@@ -1,38 +1,53 @@
 // A cipher suite of quantum safe algorithms from Open Quantum Safe
 // Users can select an algorithm for both key exchange and signing
 
-use oqs;
 use oqs::{kem, sig};
 
-pub struct Cipher {
+pub struct Persona {
+    name: String,
     kem: kem::Kem,
     sig: sig::Sig,
+    kem_keypairs: Vec<(kem::PublicKey, kem::SecretKey)>,
+    sig_keypairs: Vec<(sig::PublicKey, sig::SecretKey)>
 }
 
-impl Cipher {
-    pub fn new(kem_algo: &str, sig_algo: &str) -> Self {
+impl Persona {
+    pub fn new(name: String, kem_algo: &str, sig_algo: &str) -> Self {
+        // Initialize kem and sig algorithms
         let kem_algo = get_kem_algorithm(kem_algo);
         let sig_algo = get_sig_algorithm(sig_algo);
-
         let kem = kem::Kem::new(kem_algo).expect("Failed to create Kem object");
         let sig = sig::Sig::new(sig_algo).expect("Failed to create Sig object");
 
-        Cipher {
+        // Generate kem and sig keypairs
+        let (kem_pk, kem_sk) = kem.keypair().expect("Failed to generate keypair");
+        let mut kem_keypairs = Vec::new();
+        kem_keypairs.push((kem_pk, kem_sk));
+        let (sig_pk, sig_sk) = sig.keypair().expect("Failed to generate keypair");
+        let mut sig_keypairs = Vec::new();
+        sig_keypairs.push((sig_pk, sig_sk));
+        
+        // Create new persona
+        Persona {
+            name,
             kem,
             sig,
+            kem_keypairs,
+            sig_keypairs
         }
     }
 
-    pub fn generate_kem_keys(self) -> (kem::PublicKey, kem::SecretKey) {
+    fn generate_kem_keys(self) -> (kem::PublicKey, kem::SecretKey) {
         self.kem.keypair().expect("Failed to generate keypair")
     }
 
-    pub fn generate_sig_keys(self) -> (sig::PublicKey, sig::SecretKey) {
+    fn generate_sig_keys(self) -> (sig::PublicKey, sig::SecretKey) {
         self.sig.keypair().expect("Failed to generate keypair")
     }
 }
 
 // TODO: throw an error if no strings match
+// Can add more if we want
 fn get_kem_algorithm(kem_algo: &str) -> kem::Algorithm {
     match kem_algo {
         "BikeL1" => kem::Algorithm::BikeL1,
