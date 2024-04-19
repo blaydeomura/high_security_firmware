c0379e2d07b65219e7538d584c6382838073b65f
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-04-19 01:16:11 -0700

    updated wallet to use a single file

--- src/wallet.rs
+++ src/wallet.rs
@@ -2,15 +2,17 @@
 // Wallet contents are stored in JSON format
 // Operations include loading wallet from file, saving wallet to file, and adding and removing keys
 
-use crate::cipher_suite::{self, CipherSuite};
+use serde_json::Deserializer;
+
+use crate::cipher_suite::CS;
 use std::collections::HashMap;
-use std::fs;
-use std::path::Path;
+use std::fs::OpenOptions;
+use std::io::{Read, Write};
 
 // Wallet contains a hashmap with names of individuals and associated ciphersuite objects
 
 pub struct Wallet {
-    pub keys: HashMap<String, Box<dyn CipherSuite>>, // Maps a name to a ciphersuite object
+    pub keys: HashMap<String, CS>, // Maps a name to a ciphersuite object
 }
 
 impl Default for Wallet {
@@ -26,42 +28,65 @@ impl Wallet {
         Wallet { keys }
     }
 
-    // Opens all persona files in wallet folder and loads them into hashmap
-    pub fn load_wallet(&mut self, dir_path: String) -> std::io::Result<()> {
-        for entry in fs::read_dir(dir_path)? {
-            let dir = entry?;
-            let content = fs::read_to_string(dir.path())?;
-            let json_string = json::parse(&content).unwrap();
-            let cipher_suite = cipher_suite::deserialize_ciphersuite(json_string).unwrap();
-            self.keys
-                .insert(cipher_suite.get_name().clone(), cipher_suite);
+    pub fn load_wallet(&mut self, wallet_path: &str) -> std::io::Result<()> {
+        // Open .wallet file
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .read(true)
+            .open(wallet_path)?;
+
+        // Read contents into string
+        let mut buffer = String::new();
+        let bytes_read = file.read_to_string(&mut buffer)?;
+
+        // Deserialize ciphersuites
+        if bytes_read > 0 {
+            let stream = Deserializer::from_str(&buffer).into_iter::<CS>();
+            for cs in stream {
+                let cs = cs.expect("Unable to deserialize ciphersuite");
+                let cs_clone = cs.clone();
+                self.keys
+                    .insert(cs.to_box().get_name().to_string(), cs_clone);
+            }
         }
+
         Ok(())
     }
 
-    // Serializes a ciphersuite object and saves it in keys hashmap
-    pub fn save_ciphersuite(&mut self, cs: Box<dyn CipherSuite>) -> std::io::Result<()> {
-        let path_str = format!("wallet/{}.json", cs.get_name());
-        let path = Path::new(&path_str);
+    pub fn save_ciphersuite(&mut self, cs: CS, wallet_path: &str) -> std::io::Result<()> {
+        let mut wallet_file = OpenOptions::new().append(true).open(wallet_path)?;
+
         let serialized = serde_json::to_string_pretty(&cs)?;
-        fs::write(path, serialized)?;
-        self.keys.insert(cs.get_name().clone(), cs);
+        wallet_file.write_all(&serialized.into_bytes())?;
+        wallet_file.write(b"\n")?;
+
         Ok(())
     }
 
     // Removes a ciphersuite from local storage and keys hashmap
-    pub fn remove_ciphersuite(&mut self, name: &str) -> std::io::Result<()> {
+    pub fn remove_ciphersuite(&mut self, name: &str, wallet_path: &str) -> std::io::Result<()> {
+        let mut wallet_file = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(wallet_path)?;
         // Convert name to lower case for case-insensitive handling
         let lower_name = name.to_lowercase();
 
+        // Remove from map and reserialize wallet
         self.keys.remove(&lower_name);
-        let path_str = format!("wallet/{}.json", lower_name);
-        fs::remove_file(path_str)?;
+        let values: Vec<CS> = self.keys.values().cloned().collect();
+        for cs in values {
+            let serialized = serde_json::to_string_pretty(&cs)?;
+            wallet_file.write_all(&serialized.into_bytes())?;
+            wallet_file.write(b"\n")?;
+        }
+
         Ok(())
     }
 
     // Getter for name of persona
-    pub fn get_ciphersuite(&self, name: &str) -> Option<&Box<dyn CipherSuite>> {
-        self.keys.get(&name.to_lowercase())
+    pub fn get_ciphersuite(&self, name: &str) -> Option<CS> {
+        self.keys.get(&name.to_lowercase()).cloned()
     }
 }
